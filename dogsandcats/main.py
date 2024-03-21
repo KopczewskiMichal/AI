857e84058443f27fd268a66dc0c9d172fea6404f
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-03-21 23:49:41 +0100

    Sprawny program trenujący sieć, accuraccy lekko ponad 80%

--- dogsandcats/main.py
+++ dogsandcats/main.py
@@ -1,5 +1,6 @@
 # baseline model for the dogs vs cats dataset
 import sys
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot
 from keras.utils import to_categorical
 from keras.models import Sequential
@@ -8,7 +9,8 @@ from keras.layers import MaxPooling2D
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.optimizers import SGD
-from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import Callback
+# from keras.preprocessing.image import ImageDataGenerator
 
 # define cnn model
 def define_model():
@@ -19,7 +21,7 @@ def define_model():
 	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
 	model.add(Dense(1, activation='sigmoid'))
 	# compile model
-	opt = SGD(lr=0.001, momentum=0.9)
+	opt = SGD(learning_rate=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 	return model
 
@@ -47,18 +49,23 @@ def run_test_harness():
 	# create data generator
 	datagen = ImageDataGenerator(rescale=1.0/255.0)
 	# prepare iterators
-	train_it = datagen.flow_from_directory('dataset_dogs_vs_cats/train/',
+	train_it = datagen.flow_from_directory('dogs-vs-cats/train/',
 		class_mode='binary', batch_size=64, target_size=(200, 200))
-	test_it = datagen.flow_from_directory('dataset_dogs_vs_cats/test/',
+	test_it = datagen.flow_from_directory('dogs-vs-cats/validation/',
 		class_mode='binary', batch_size=64, target_size=(200, 200))
 	# fit model
-	history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
-		validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=0)
+	print("Zaraz rozpoczynamy trenowanie")
+	history = model.fit(train_it, steps_per_epoch=len(train_it),
+		validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=1)
 	# evaluate model
-	_, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+	_, acc = model.evaluate(test_it, steps=len(test_it), verbose=1)
 	print('> %.3f' % (acc * 100.0))
+	model.save('trained_model.h5')
+	# model.save('trained_model.keras')
+
 	# learning curves
 	summarize_diagnostics(history)
+
 
 # entry point, run the test harness
 run_test_harness()
