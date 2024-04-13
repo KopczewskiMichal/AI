11bf93a63d3b422a0baa825c7ff5b22194b325a6
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-04-13 15:45:28 +0200

    30% accuracy emocji, dla bardzo małego obrazka

--- emocje/train.py
+++ emocje/train.py
@@ -11,18 +11,20 @@ from keras.optimizers import SGD
 from keras.callbacks import Callback
 from keras.callbacks import ModelCheckpoint
 
+images_width_height = 100
+
 def define_model():
 	model = Sequential()
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(224, 224, 1)))
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(images_width_height, images_width_height, 1)))
 	model.add(MaxPooling2D((2, 2)))
 	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 	model.add(MaxPooling2D((2, 2)))
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-	model.add(Dense(1, activation='sigmoid'))
+	model.add(Dense(8, activation='softmax'))
 	# compile model
-	opt = SGD(learning_rate=0.001, momentum=0.9)
-	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+	# opt = SGD(learning_rate=0.001, momentum=0.9) # to możliwy optimizer
+	model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 	return model
 
 def summarize_diagnostics(history):
@@ -46,9 +48,9 @@ def run_test_harness():
 	datagen = ImageDataGenerator(rescale=1.0/255.0)
 	# prepare iterators
 	train_it = datagen.flow_from_directory('Dataset/train/',
-		class_mode='categorical', batch_size=64, target_size=(224, 224))
+		class_mode='categorical', batch_size=64, target_size=(images_width_height, images_width_height), color_mode='grayscale')
 	test_it = datagen.flow_from_directory('Dataset/validation/',
-		class_mode='categorical', batch_size=64, target_size=(224, 224))
+		class_mode='categorical', batch_size=64, target_size=(images_width_height, images_width_height), color_mode='grayscale')
 
 	checkpoint = ModelCheckpoint("best_model.keras", monitor='accuracy', verbose=1,
     save_best_only=True, mode='auto')
@@ -61,8 +63,10 @@ def run_test_harness():
 	# evaluate model
 	_, acc = model.evaluate(test_it, steps=len(test_it), verbose=1)
 	print('> %.3f' % (acc * 100.0))
-	# model.save('trained_model.keras')
 
 	# learning curves
 	summarize_diagnostics(history)
 
+
+if __name__ == '__main__':
+  run_test_harness()
