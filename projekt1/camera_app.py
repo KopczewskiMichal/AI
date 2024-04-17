32107fa88a59cdeb07bd76f2af805ca048d5b381
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-04-17 11:26:46 +0200

    Dodano liczenie macierzy błędów modelu

--- projekt1/camera_app.py
+++ projekt1/camera_app.py
@@ -6,7 +6,6 @@ def main():
   model = load_model('emotions.keras', compile=False)
   model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
 
-  # model.summary()
   emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
   face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -17,18 +16,20 @@ def main():
       if not ret:
           break
       gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      num_faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+      num_faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=4) # minNeighbors pierwotnie 4
 
       for (x, y, w, h) in num_faces:
-          # cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-          cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+          cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+          # cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
           roi_gray_frame = gray_frame[y:y + h, x:x + w]
           cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
-          emotion_prediction = model.predict(cropped_img)
+          emotion_prediction = model.predict(cropped_img, verbose=0)
+          # print(propability)
           maxindex = int(np.argmax(emotion_prediction))
-          cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+          propability = round(emotion_prediction[0][maxindex], 2)
+          cv2.putText(frame, f"{emotion_dict[maxindex]} {propability * 100}%", (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-      cv2.imshow('Video', cv2.resize(frame, (1200, 860), interpolation=cv2.INTER_CUBIC))
+      cv2.imshow('Video', cv2.resize(frame, (2400, 1800), interpolation=cv2.INTER_CUBIC))
       
       # Wait for the 'q' key press to exit the loop
       if cv2.waitKey(1) & 0xFF == ord('q'):
---
+++ projekt1/create_model_stats.py
@@ -0,0 +1,37 @@
+from keras.models import load_model
+from train import define_data_generator
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+def main():
+  model = load_model('emotions.keras', compile=False)
+  model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
+
+  data_generator = define_data_generator("Dataset/test")
+
+  compute_confusion_matrix(model, data_generator)
+
+
+def compute_confusion_matrix(model, generator):
+  predicted_labels = np.argmax(model.predict(generator), axis=1)
+
+  true_labels = generator.classes
+
+  conf_matrix = confusion_matrix(true_labels, predicted_labels)
+
+  plt.figure(figsize=(8, 6))
+  plt.imshow(conf_matrix, cmap=plt.cm.Blues)
+  plt.colorbar()
+  plt.title('Confusion Matrix')
+  plt.xlabel('Predicted Labels')
+  plt.ylabel('True Labels')
+  plt.xticks(np.arange(len(generator.class_indices)), generator.class_indices.keys(), rotation=45)
+  plt.yticks(np.arange(len(generator.class_indices)), generator.class_indices.keys())
+  plt.tight_layout()
+  plt.savefig("plots/confusion_matrix.png")
+  plt.show()
+
+
+if __name__ == '__main__':
+  main()
