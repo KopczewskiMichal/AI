bb8aa028049f3c1918cbc8e7c23388c035fe3706
Michał Kopczewski <kopczewski.michal101@gmail.com>
2024-05-10 16:20:17 +0200

    lab7 zad 1 skończone

--- lab7/1.py
+++ lab7/1.py
@@ -1,12 +1,12 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 
 def main():
   stop_words = stopwords.words('english')
-  stop_list = [',', '.', '“', '”', '’', "'s"]
-  stop_words.extend(stop_list)
   
   document: list = readDocument("article.txt")
   tokens = nltk.word_tokenize(document)
@@ -14,10 +14,17 @@ def main():
 
   filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
   print(f"Ilość słów po usunięciu stop_words: {len(filtered_sentence)}")
+  
+  stop_words.extend([',', '.', '“', '”', '’', "'s"])
+  
+  filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
+  print(f"Ilość słów po usunięciu dodanych stop_words: {len(filtered_sentence)}")
+  
   lemmas = lemmatize(filtered_sentence)
   print(f"Ilość słów po lematyzacji: {len(lemmas)}")
   
-  count_words(lemmas)
+  # count_words(lemmas)
+  word_cloud(lemmas)
   
     
 def lemmatize(filtered_sentence: list) -> list:
@@ -36,6 +43,20 @@ def count_words(tokens):
   fdist1 = nltk.FreqDist(tokens)
   print (fdist1.most_common(10))
   fdist1.plot(20)
+  
+def word_cloud(tokens):
+  filtered_text = " ".join(tokens)
+    
+  wordcloud = WordCloud(width = 800, height = 800, 
+                  background_color ='white', 
+                  min_font_size = 10).generate(filtered_text)
+
+  plt.figure(figsize = (8, 8), facecolor = None) 
+  plt.imshow(wordcloud) 
+  plt.axis("off") 
+  plt.tight_layout(pad = 0) 
+
+  plt.show()
 
 
 
