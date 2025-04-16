8569e2d290a7a998c6a3ac7a916eb4d921d226ba
kopcz <kopczewski.michal101@gmail.com>
2025-04-16 12:32:45 +0200

    Naprawione wczytywanie do preprocessingu LSTM przed alg genetycznym

--- projekt2/preprocessing.py
+++ projekt2/preprocessing.py
@@ -10,26 +10,32 @@ from tensorflow.keras.models import load_model
 
 
 def predictLSTM(df: pandas.DataFrame) -> dict:
+    print(df.head())
     LOOK_BACK = 3
-    tickers = df["ticker"].unique()
+    tickers = df.columns.drop('Date')
+
 
     model = load_model(f"LSTMmodel{LOOK_BACK}.keras")
     scaler = MinMaxScaler(feature_range=(0, 1))
     result = {}
     for act_ticker in tickers:
-        new_dataframe = df.loc[df['ticker'] == act_ticker]
-        close_values = new_dataframe['Close'].values
+        print(len(result))
+        close_values = df[act_ticker].values
+        # Now you can work with the close_values for each ticker
+        # print(f"Ticker: {act_ticker}, Values: {close_values}")
+
+
         close_values = close_values.reshape(-1, 1)  # Reshape to 2D
         close_values_scaled = scaler.fit_transform(close_values)
-        # Ważnym aby dokonywać predykcji tylko na kolejny dzień.
+        # Predykcja zmiany w kolejnym dniu
         recent_close_values_scaled = close_values_scaled[-LOOK_BACK:]
         newX = np.reshape(recent_close_values_scaled, (1, 1, LOOK_BACK))
         newPredict = model.predict(newX)
-        # Invert prediction
         newPredict = scaler.inverse_transform(newPredict)
         tomorrow_prediction = newPredict[0][0]
         result[act_ticker] = tomorrow_prediction / close_values[-1]
 
+    print(result)
     return result
 
 def create_dataset(dataset, time_step=1):
@@ -42,7 +48,7 @@ def create_dataset(dataset, time_step=1):
 
 
 if __name__ == '__main__':
-    datafile = 'resources/new_data.csv'  # Replace with your new CSV file path
+    datafile = 'resources/stock_data.csv'
     df = read_csv(datafile, engine='python')
     predictions = predictLSTM(df)
     print(predictions)
